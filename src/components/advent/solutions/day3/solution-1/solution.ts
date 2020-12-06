874e0cf37996bd90ab1d3c30626db8f83d9226c4
Vlad Silin <vladislavs.silins@inbox.lv>
2020-12-06 20:34:27 +0200

    feat: day 3 solved

--- src/components/advent/solutions/day3/solution-1/solution.ts
+++ src/components/advent/solutions/day3/solution-1/solution.ts
@@ -1,27 +1,59 @@
 /**
-Advent of Code[About][Events][Shop][Settings][Log Out]Vlad Silin 2*
-       y(2020)[Calendar][AoC++][Sponsors][Leaderboard][Stats]
+Advent of Code[About][Events][Shop][Settings][Log Out]Vlad Silin 4*
+ {'year':2020}[Calendar][AoC++][Sponsors][Leaderboard][Stats]
 Our sponsors help make Advent of Code possible:
-GitHub - We're hiring engineers to make GitHub fast. Interested? Email fast@github.com with details of exceptional performance work you've done in the past.
---- Day 2: Password Philosophy ---
-Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via toboggan.
+Scott Logic - Check out our tech blog if you're interested in us. And from all of us at Scott Logic, have a great and safe Christmas!
+--- Day 3: Toboggan Trajectory ---
+With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
 
-The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something's wrong with our computers; we can't log in!" You ask if you can take a look.
+Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
 
-Their password database seems to be a little corrupted: some of the passwords wouldn't have been allowed by the Official Toboggan Corporate Policy that was in effect when they were chosen.
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
 
-To try to debug the problem, they have created a list (your puzzle input) of passwords (according to the corrupted database) and the corporate policy when that password was set.
+..##.........##.........##.........##.........##.........##.......  --->
+#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........#.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...##....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
 
-For example, suppose you have the following list:
+The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
 
-1-3 a: abcde
-1-3 b: cdefg
-2-9 c: ccccccccc
-Each line gives the password policy and then the password. The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
+From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
 
-In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
+The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
 
-How many passwords are valid according to their policies?
+..##.........##.........##.........##.........##.........##.......  --->
+#..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........X.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...#X....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+In this example, traversing the map using this slope would cause you to encounter 7 trees.
+
+Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
 
 To begin, get your puzzle input.
 
@@ -29,41 +61,32 @@ Answer:
 
 
 You can also [Share] this puzzle.
+
+The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1: From your starting position at the top-left, check the position Перевести
+opted
+
  */
 
 import raw from "raw.macro";
 import { puzzleTransform } from "../../../utils/puzzle-transform";
-import { toNumber, isNumber } from "lodash";
+import { repeat, truncate } from "lodash";
 
-interface PasswordInfo {
-  min: number;
-  max: number;
-  letter: string;
-  password: string;
-}
-const parser = (data: string): PasswordInfo => {
-  const [range, rawLetter, password] = data.split(" ");
-  const [min, max] = range.split("-");
-  const [letter] = rawLetter.split(":");
-  return {
-    min: toNumber(min),
-    max: toNumber(max),
-    letter,
-    password,
-  };
-};
 export const solution = (): number => {
-  const input = puzzleTransform(raw("./puzzle.txt")).map((item) =>
-    parser(item)
-  );
+  const input = puzzleTransform(raw("./puzzle.txt"));
   let count = 0;
-  for (const checkedPassword of input) {
-    const { min, max, letter, password } = checkedPassword;
-    const letterInPasswordCount = password.split(letter).length - 1;
-    if (letterInPasswordCount >= min && letterInPasswordCount <= max) {
+  const terrainLength = input[0].length;
+  for (let i = 0; i < input.length; i++) {
+    const position = (i * 3) % (terrainLength - 1);
+
+    const terrain = input[i];
+    if (terrain[position] === "#") {
       count++;
-      console.log(checkedPassword);
     }
+    console.log(i, position, terrainLength);
+    console.log(input[i]);
+    console.log(terrain);
+    console.log(truncate(terrain, { length: position + 1, omission: "" }));
+    console.log(terrain[position]);
   }
   return count;
 };
--- src/components/advent/solutions/day3/solution-2/solution.ts
+++ src/components/advent/solutions/day3/solution-2/solution.ts
@@ -1,48 +1,80 @@
 /**
-Advent of Code[About][Events][Shop][Settings][Log Out]Vlad Silin 3*
-   $year=2020;[Calendar][AoC++][Sponsors][Leaderboard][Stats]
+Advent of Code[About][Events][Shop][Settings][Log Out]Vlad Silin 5*
+  0.0.0.0:2020[Calendar][AoC++][Sponsors][Leaderboard][Stats]
 Our sponsors help make Advent of Code possible:
 GitHub - We're hiring engineers to make GitHub fast. Interested? Email fast@github.com with details of exceptional performance work you've done in the past.
---- Day 2: Password Philosophy ---
-Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via toboggan.
+--- Day 3: Toboggan Trajectory ---
+With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
 
-The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something's wrong with our computers; we can't log in!" You ask if you can take a look.
+Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
 
-Their password database seems to be a little corrupted: some of the passwords wouldn't have been allowed by the Official Toboggan Corporate Policy that was in effect when they were chosen.
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
 
-To try to debug the problem, they have created a list (your puzzle input) of passwords (according to the corrupted database) and the corporate policy when that password was set.
+..##.........##.........##.........##.........##.........##.......  --->
+#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........#.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...##....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
 
-For example, suppose you have the following list:
+The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
 
-1-3 a: abcde
-1-3 b: cdefg
-2-9 c: ccccccccc
-Each line gives the password policy and then the password. The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
+From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
 
-In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
+The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
 
-How many passwords are valid according to their policies?
+..##.........##.........##.........##.........##.........##.......  --->
+#..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........X.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...#X....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+In this example, traversing the map using this slope would cause you to encounter 7 trees.
 
-Your puzzle answer was 506.
+Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+
+Your puzzle answer was 159.
 
 The first half of this puzzle is complete! It provides one gold star: *
 
 --- Part Two ---
-While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
 
-The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
 
-Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+Right 1, down 1.
+Right 3, down 1. (This is the slope you already checked.)
+Right 5, down 1.
+Right 7, down 1.
+Right 1, down 2.
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
 
-Given the same example list from above:
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
 
-1-3 a: abcde is valid: position 1 contains a and position 3 does not.
-1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
-2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
-How many passwords are valid according to the new interpretation of the policies?
-
-Answer:
-
+Answer: 
+ 
 
 Although it hasn't changed, you can still get your puzzle input.
 
@@ -51,51 +83,37 @@ You can also [Share] this puzzle.
 
 import raw from "raw.macro";
 import { puzzleTransform } from "../../../utils/puzzle-transform";
-import { toNumber, isNumber } from "lodash";
-
-interface PasswordInfo {
-  min: number;
-  max: number;
-  letter: string;
-  password: string;
-}
-const parser = (data: string): PasswordInfo => {
-  const [range, rawLetter, password] = data.split(" ");
-  const [min, max] = range.split("-");
-  const [letter] = rawLetter.split(":");
-  return {
-    min: toNumber(min),
-    max: toNumber(max),
-    letter,
-    password,
-  };
-};
-
-const replaceSymbol = (
-  word: string,
-  symbol: string,
-  position: number
-): string => {
-  const letteredWord = word.split("");
-  letteredWord[position] = symbol;
-  return letteredWord.join("");
-};
-
-export const solution = (): number => {
-  const input = puzzleTransform(raw("./puzzle.txt")).map((item) =>
-    parser(item)
-  );
+import { repeat, truncate } from "lodash";
+const trySlope = (right: number, down: number): number => {
+  const input = puzzleTransform(raw("./puzzle.txt"));
   let count = 0;
-  for (const checkedPassword of input) {
-    const { min, max, letter, password } = checkedPassword;
-    const minReplacePassword = replaceSymbol(password, letter, min - 1);
-    const maxReplacePassword = replaceSymbol(password, letter, max - 1);
-    const isMinPosition = password === minReplacePassword;
-    const isMaxPosition = password === maxReplacePassword;
-    if (!(isMaxPosition && isMinPosition) && (isMaxPosition || isMinPosition)) {
+  const terrainLength = input[0].length;
+  for (let i = 0; i < input.length; i = i + down) {
+    const position = ((i / down) * right) % (terrainLength - 1);
+
+    const terrain = input[i];
+    if (terrain[position] === "#") {
       count++;
-      console.log(checkedPassword);
     }
+    // console.log(i, position, terrainLength);
+    // console.log(input[i]);
+    // console.log(terrain);
+    // console.log(truncate(terrain, { length: position + 1, omission: "" }));
+    // console.log(terrain[position]);
   }
   return count;
+};
+export const solution = (): number => {
+  console.log(trySlope(1, 1));
+  console.log(trySlope(3, 1)); //159
+  console.log(trySlope(5, 1));
+  console.log(trySlope(7, 1));
+  console.log(trySlope(1, 2));
+  return (
+    trySlope(1, 1) *
+    trySlope(3, 1) *
+    trySlope(5, 1) *
+    trySlope(7, 1) *
+    trySlope(1, 2)
+  );
 };
